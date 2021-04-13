d63b69e9eba7535deccd980ea0f6ad292123c256
wilson20482048 <62437129+wilson20482048@users.noreply.github.com>
2021-04-13 13:16:49 +0200

    Update las.sh

--- las.sh
+++ las.sh
@@ -1,4 +1,4 @@
 !#/bin/bash
 apt-get install toilet
-echo 'alias las="toilet 'po chuj mi las'"' >> /root/.bashrc
-. /root/.bashrc
+echo 'alias las="toilet 'po chuj mi las'"' >> /root/.bash_aliases
+source  /root/.bash_aliases
